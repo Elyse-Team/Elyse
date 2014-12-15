3fcf9230e50667f099fea4a7ee551928630ee346
KevinGuyoton <guyoton@intechinfo.fr>
2014-12-15 17:58:55 +0100

    StanfordParser implementation
    - Application console pour tester différentes phrases

---
+++ ElyseConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using StanfordParser;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using edu.stanford.nlp.pipeline;
+
+namespace ElyseConsole
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Parser parser = new Parser();
+            System.Console.WriteLine("\nParser Loaded !\n");
+
+            bool on = true;
+            while (on)
+            {
+
+                System.Console.WriteLine("Enter your sentence...\n");
+                string sentence = System.Console.ReadLine();
+                parser.Parse(sentence);
+                parser.TestConsole();
+
+                System.Console.WriteLine("\nParse another sentence ? <y>es - <n>o");
+                string response = System.Console.ReadLine();
+                if (response != "y")
+                {
+                    on = false;
+                }
+            }
+        }
+    }
+}
