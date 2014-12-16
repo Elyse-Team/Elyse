c67996d75974860928d1dd47b591b41c91728539
lighta971 <lighta971@live.fr>
2014-12-17 00:31:18 +0100

    add write languagetool squeleton tests

--- Languagetool.Tests/Languagetool.cs
+++ Languagetool.Tests/Languagetool.cs
@@ -8,9 +8,15 @@ namespace Languagetool.Tests
     public class Languagetool
     {
         [TestMethod]
-        public void GetErrors_withErrorMatches_returnErrors()
+        public void GetErrors_withErrorMatches_returnErrorList()
         {
             Elyse.Languagetool.Languagetool ltool = new Elyse.Languagetool.Languagetool("http://localhost:8081/");
+
+        }
+
+        [TestMethod]
+        public void GetErrors_withNoErrorMatches_returnEmptyErrorList()
+        {
 
         }
     }
